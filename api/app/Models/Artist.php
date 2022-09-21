38e8dd4a6e77c3bf7f39da01e3efdbc77c61ee82
Shuhei-Kubo <shuhei.k6221@gmail.com>
2022-09-21 22:12:16 +0900

    マイグレーションファイル、モデル追加

---
+++ api/app/Models/Artist.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Artist extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+    protected $fillable = [
+        'name', 
+        'profile', 
+        'nationality',
+        'image',
+    ];
+
+    public function liveDetails(){
+        return $this->morphToMany(LiveDetail::class, 'playable');
+    }
+}
